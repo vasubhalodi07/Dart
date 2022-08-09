ff71c68a7b993c89d04c3aefdad0abeafbd1fe86
Vasu Bhalodi <88498558+vasubhalodi07@users.noreply.github.com>
2022-08-09 21:58:59 +0530

    Update parameters.dart

--- functions/parameters.dart
+++ functions/parameters.dart
@@ -18,7 +18,7 @@ void f2(int g1, { int? g2, int? g3 })
     print("g3 is $g3");
 }
 
-void f3(int g1, { int g2 : 12 })
+void f3(int g1, { int g2? : 12 })
 {
     print("g1 is $g1");
     print("g2 is $g2");
